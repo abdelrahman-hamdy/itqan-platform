e400340c4a44c7383503a03642836b4a5aecd4c0
abdelrahman hamdy <abdelrahmanhamdy320@gmail.com>
2025-12-16 19:25:39 +0200

    Simplify Coming Soon page to basic message

--- app/Filament/Resources/PaymentResource/Pages/ListPayments.php
+++ app/Filament/Resources/PaymentResource/Pages/ListPayments.php
@@ -4,7 +4,6 @@ namespace App\Filament\Resources\PaymentResource\Pages;
 
 use App\Filament\Resources\PaymentResource;
 use Filament\Resources\Pages\Page;
-use Filament\Actions;
 
 class ListPayments extends Page
 {
@@ -13,20 +12,4 @@ class ListPayments extends Page
     protected static string $view = 'filament.pages.coming-soon';
 
     protected static ?string $title = 'المدفوعات';
-
-    public function getViewData(): array
-    {
-        return [
-            'title' => 'نظام المدفوعات',
-            'description' => 'نعمل حالياً على تطوير نظام متكامل لإدارة المدفوعات يتضمن:',
-            'features' => [
-                'ربط بوابات الدفع الإلكتروني (مدى، فيزا، ماستركارد)',
-                'إدارة الفواتير والإيصالات',
-                'تتبع المدفوعات والمستحقات',
-                'تقارير مالية شاملة',
-                'نظام تذكيرات للمدفوعات المتأخرة',
-            ],
-            'icon' => 'heroicon-o-banknotes',
-        ];
-    }
 }
